327eb87668b4763e68da2c460913a8ebf5758437
NADYASYAHPUTRI <nadyasyahputri1226@gmail.com>
2025-05-01 20:13:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import os
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -36,4 +38,37 @@ async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
+@bot.command('meme')
+async def mem(ctx):
+    img_name=random.choice(os.listdir('discord/images'))
+   # Dan inilah cara Kamu mengganti nama file dari variabel!
+    with open(f"discord/images/{img_name}", 'rb') as f:
+            picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('kerajinan')
+async def art(ctx):
+    img_name=random.choice(os.listdir('discord/images/art'))
+   # Dan inilah cara Kamu mengganti nama file dari variabel!
+    with open(f"discord/images/art/{img_name}", 'rb') as f:
+            picture = discord.File(f)
+   # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
 bot.run("TOKEN")
+
+
